13a0b21943176a60bd4ebe4041b784532912734c
Pain23real <example@example.com>
2025-06-04 23:53:31 +0300

    Updated React rendering method for React 17 compatibility

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import './process-polyfill';
 
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -21,11 +21,11 @@ if (typeof window.Buffer === 'undefined') {
   window.Buffer = require('buffer/').Buffer;
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
